473f4e89fb9394ccd6366a883c6eff42fbf20355
sejals2 <shingotesejal2004@gmail.com>
2024-09-12 19:49:24 +0530

    commit

---
+++ patterns/Ex1.java
@@ -0,0 +1,21 @@
+/* SOLID RECTANGLE 
+   *****
+   *****
+   *****
+   ***** */
+
+package patterns;
+
+public class Ex1 {
+    public static void main(String[]args){
+        
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ patterns/Ex9.java
@@ -0,0 +1,28 @@
+/*TRIANGLE
+1
+01
+101
+0101
+10101
+ */
+package patterns;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
